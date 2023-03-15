5749cc90a792480fedc4e5cb6ba194d0f49854e2
Youssef <77765058+jaedyxn@users.noreply.github.com>
2023-03-15 19:07:32 +0100

    Create preprocess.py

---
+++ preprocess.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv("clean.csv")
+
+'''
+80:20 train test split
+'''
+train, test = train_test_split(df, test_size=.2, random_state=42, stratify=df['FLAG'])
+
+train.to_csv("train.csv")
+test.to_csv("test.csv")
